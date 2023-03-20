f2452a851ac507367ddb33f87b035e209eec4c04
PhilippeVSC <philippe.vivesc@gmail.com>
2023-03-20 22:59:37 +0100

    Nettoyage global, ajout de commentaires

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 /** Navbar **/
-
 window.onscroll = function () {
+    /** Show the Header **/
     if (window.scrollY >= 60 || window.pageYOffset >= 60){
         document.getElementsByTagName("header")[0].classList.add("header--scroll");
         document.getElementsByTagName("nav")[0].classList.add("nav--scroll");
@@ -8,6 +8,7 @@ window.onscroll = function () {
         document.getElementsByTagName("header")[0].classList.remove("header--nscroll");
         document.getElementsByTagName("nav")[0].classList.remove("nav--nscroll");        
     } else {
+    /** Hide the Header on #Home **/
         document.getElementsByTagName("header")[0].classList.add("header-nscroll");
         document.getElementsByTagName("nav")[0].classList.add("nav--nscroll");
 
@@ -17,15 +18,16 @@ window.onscroll = function () {
 };
 
 /** Modal CV window **/
+let openModal = document.getElementsByClassName("CV_button")[0]; /** Opening Button **/
+let closeModal = document.getElementsByClassName("modal_close_button_img")[0]; /** Closing Button **/
+let modal = document.getElementsByClassName("modal_CV")[0]; /** Modal Div **/
 
-let openModal = document.getElementsByClassName("CV_button")[0];
-let closeModal = document.getElementsByClassName("modal_close_button_img")[0];
-let modal = document.getElementsByClassName("modal_CV")[0];
-
+/** Opening button on click display the modal div */
 openModal.onclick = () => {
     modal.style.display = 'block';
 }
 
+/** Closing button on click hide the modal div */
 closeModal.onclick = () => {
     modal.style.display = 'none';
 }
